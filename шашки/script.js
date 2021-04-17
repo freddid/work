b775fe9159b93772cd4a774cea0cdd7ac7c1d76b
fred <fredded21@gmail.com>
2021-04-17 14:17:55 +0300

    phh

--- шашки/script.js
+++ шашки/script.js
@@ -1,3 +1,54 @@
+$(function () {
+   arr_EN = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', ''];
+   for (let i = 0; i < 10; i++) {
+      for (let a = 0; a < 10; a++) {
+         $('.board').append("<span class = 'fred'><div> <span class = 'dot'> </span></div></span>")
+         let f = i * 10 + a
+         $($(".fred")[f]).attr('id', f)
+         if (a == 0 || a == 9) {
+            $($(".fred")[f]).text(i).addClass("letter")
+         }
+         if (i == 0 || i == 9) {
+            $($(".fred")[f]).text(arr_EN[a]).addClass("letter")
+         }
+         $(".fred").toggleClass("black")
+         if (i > 0 && i < 4) {
+            $(".fred div").toggleClass('white dad')
+         }
+         if (i > 5 && i < 10) {
+            $(".fred div").toggleClass('dark dad')
+         }
+      }
+      $(".fred").toggleClass("black")
+      if (i > 0 && i < 4) {
+         $(".fred div").toggleClass('white dad')
+      }
+      if (i > 5 && i < 10) {
+         $(".fred div").toggleClass('dark dad')
+      }
+   }
+
+   $('.dad').on('click', function () {
+      let ths = $(this)
+      $('span').removeClass('active')
+      let id = +$(this).parent(".fred").attr('id'),
+         pos2 = id - 9,
+         pos4 = id - 11;
+         
+      poss = [pos2, pos4];
+      for (let pos of poss) {
+         if ($('#' + pos + ' div').attr('class') == '') {
+            $('#' + pos + ' span').addClass('active')
+         }
+      }
+      $('.active').on('click', function () {
+         $(this).parent('div').addClass('dad dark')
+         $('.active').removeClass('active')
+         ths.removeClass('dad dark')
+      })
+   })
+
+})
 // let r = 10,
 //    board = '',
 //    boardHTML = document.querySelector('.board'),
@@ -65,54 +116,3 @@
 //    }
 // }
 
-$(function () {
-   arr_EN = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', ''];
-   for (let i = 0; i < 10; i++) {
-      for (let a = 0; a < 10; a++) {
-         $('.board').append("<span class = 'fred'><div> <span class = 'dot'> </span></div></span>")
-         let f = i * 10 + a
-         $($(".fred")[f]).attr('id', f)
-         if (a == 0 || a == 9) {
-            $($(".fred")[f]).text(i).addClass("letter")
-         }
-         if (i == 0 || i == 9) {
-            $($(".fred")[f]).text(arr_EN[a]).addClass("letter")
-         }
-         $(".fred").toggleClass("black")
-         if (i > 0 && i < 4) {
-            $(".fred div").toggleClass('white dad')
-         }
-         if (i > 5 && i < 10) {
-            $(".fred div").toggleClass('dark dad')
-         }
-      }
-      $(".fred").toggleClass("black")
-      if (i > 0 && i < 4) {
-         $(".fred div").toggleClass('white dad')
-      }
-      if (i > 5 && i < 10) {
-         $(".fred div").toggleClass('dark dad')
-      }
-   }
-
-   $('.dad').on('click', function () {
-      let ths = $(this)
-      $('span').removeClass('active')
-      let id = +$(this).parent(".fred").attr('id'),
-         pos2 = id - 9,
-         pos4 = id - 11;
-         
-      poss = [pos2, pos4];
-      for (let pos of poss) {
-         if ($('#' + pos + ' div').attr('class') == '') {
-            $('#' + pos + ' span').addClass('active')
-         }
-      }
-      $('.active').on('click', function () {
-         $(this).parent('div').addClass('dad dark')
-         $('.active').removeClass('active')
-         ths.removeClass('dad dark')
-      })
-   })
-
-})
